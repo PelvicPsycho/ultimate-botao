88586718260eb2f4ed8bab1a89c91170d6f1343f
PelvicPsycho <39059827+PelvicPsycho@users.noreply.github.com>
2026-04-06 17:36:19 -0300

    alguns fix pra rodar esse merge

--- Componentes/Ball/ball.gd
+++ Componentes/Ball/ball.gd
@@ -4,6 +4,6 @@ class_name Ball
 var lastTouch: Player
 
 func _on_body_entered(body):
-	if body.is_in_group("pecas") or body is Player:
+	if body.is_in_group("Players") or body is Player:
 		print('Tocou em ' + str(body))
 		lastTouch = body
--- Scripts Globais/main.gd
+++ Scripts Globais/main.gd
@@ -3,33 +3,6 @@ extends Node3D
 enum ModoTiro { PUXAR, EMPURRAR, MODO_3 }
 var modo_atual = ModoTiro.PUXAR
 
-func _on_button_pressed() -> void:
-	# Alterna entre os modos (por enquanto 0 e 1, o 2 deixaremos pronto)
-	modo_atual = (modo_atual + 1) % 3 
-	
-	var texto_botao = ""
-	match modo_atual:
-		ModoTiro.PUXAR:
-			texto_botao = "Modo: Puxar"
-		ModoTiro.EMPURRAR:
-			texto_botao = "Modo: Empurrar"
-		ModoTiro.MODO_3:
-			texto_botao = "Modo: Carregar"
-			
-	$CanvasLayer/Button.text = texto_botao
-	
-	# Avisa todas as peças do jogo qual é o novo modo
-	get_tree().call_group("pecas", "set_modo_tiro", modo_atual)
-
-
-func _on_button_puxar_pressed() -> void:
-	_on_button_pressed()
-
-
-func _on_botao_restart_pressed() -> void:
-	get_tree().reload_current_scene()
-	
-
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
