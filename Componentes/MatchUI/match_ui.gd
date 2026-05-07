17576bde3487f50ab0495826b399bf4954abab71
JoaoPioner <51408829+JoaoPioner@users.noreply.github.com>
2026-05-07 01:24:41 -0300

    [BUG] [P1] Emblemas xumbados + Novos Emblemas

--- Componentes/MatchUI/match_ui.gd
+++ Componentes/MatchUI/match_ui.gd
@@ -22,6 +22,9 @@ var current_turn_value: int = 0
 @export var team_barra_Lance: TextureProgressBar
 @export var timer_background: TextureRect
 
+@onready var HomeEmblem = $MarginContainer/Control/EmblemaEsquerda/TextureRect
+@onready var AwayEmblem = $MarginContainer/Control/EmblemaDireita/TextureRect
+
 func UI_start(team_home, team_away) -> void:
 	lateral_time_esq.modulate = team_home.cor
 	lateral_time_dir.modulate = team_away.cor
@@ -31,6 +34,8 @@ func UI_start(team_home, team_away) -> void:
 	nome_time_dir.text = team_away.name
 	time_home = team_home
 	time_away = team_away
+	HomeEmblem.texture = team_home.emblem
+	AwayEmblem.texture = team_away.emblem
 
 func colorir_turno(time_jogando, lances) -> void:
 	# A lateral continua com modulate, pois é inteira de uma cor só
