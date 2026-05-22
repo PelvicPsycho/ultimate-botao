e9ef60c503017280c23542d79cdda4f4d901588e
PelvicPsycho <39059827+PelvicPsycho@users.noreply.github.com>
2026-05-22 17:06:28 -0300

    fix das cartas bugando

--- Componentes/PlayerV2/BotoesUI.gd
+++ Componentes/PlayerV2/BotoesUI.gd
@@ -1,43 +1,59 @@
 extends Control
 
 var piece_alvo: Player
-var carta1
-var carta2
+var carta1: CardResource
+var carta2: CardResource
 var cartas: Array = []
+
 @onready var botao1: Button = $Botao1
 @onready var botao2: Button = $Botao2
+
 func _ready():
 	mouse_filter = Control.MOUSE_FILTER_STOP
 	visible = false   
+
 func definir_piece(p):
 	piece_alvo = p
 	botao1.add_theme_font_size_override("font_size", 10)
 	botao2.add_theme_font_size_override("font_size", 10)
-	# Leia as cartas DIRETAMENTE do playerInfo
-	var slots :Array = p.playerInfo.slotsUpgrates
+	
+	var slots = p.playerInfo.slotsUpgrates 
+	# Se a linha acima der erro, volte para: var slots = piece_alvo.slotsUpgrates
 
-	if slots.size() > 0:
+	# --- TRAVA DO BOTÃO 1 ---
+	if slots.size() > 0 and slots[0] != null:
 		carta1 = slots[0]
 		botao1.text = carta1.nome
+		botao1.disabled = false
+		botao1.visible = true
 	else:
 		carta1 = null
-		botao1.text = "Sem Carta"
-	if slots.size() > 1:
+		botao1.text = "Vazio"
+		botao1.disabled = true
+		botao1.visible = false
+
+	# --- TRAVA DO BOTÃO 2 ---
+	if slots.size() > 1 and slots[1] != null:
 		carta2 = slots[1]
-		botao2.text = carta2.nome 
+		botao2.text = carta2.nome
+		botao2.disabled = false
+		botao2.visible = true
 	else:
 		carta2 = null
-		botao2.text = "Sem Carta"
+		botao2.text = "Vazio"
+		botao2.visible = false
+
 func definir_cartas(lista):
 	cartas = lista
+
 func _on_botao_1_pressed():
 	if piece_alvo and carta1:
 		var ms = get_tree().get_root().get_node("MatchScene")
 		ms.tentar_usar_carta(piece_alvo, carta1)
-		
 	else:
 		print("Sem carta no slot 1")
 	visible = false
+
 func _on_botao_2_pressed():
 	if piece_alvo and carta2:
 		var ms = get_tree().get_root().get_node("MatchScene")
@@ -45,4 +61,3 @@ func _on_botao_2_pressed():
 	else:
 		print("Sem carta no slot 2")
 	visible = false
-	
